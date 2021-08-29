a9c6784ee7403309225f633b85d217d5755356a8
rdfgdfgdfg <jar20070603@163.com>
2021-08-30 07:40:08 +0800

    add an easiest example

---
+++ example/example0.cpp
@@ -0,0 +1,29 @@
+﻿// example.cpp: 定义应用程序的入口点。
+//
+
+#include "example.h"
+
+using namespace TUT::format;
+
+int main()
+{
+	String str(L"hello");//create a box to save string
+	const wchar_t* wstr = L"hello!!!!!!!!!!!!!!!!";//The matched C wstring
+	str.match(wstr);//The box will try to match from start
+	
+	wchar_t* result = str.moveStrResult();//get the result with ownership
+	std::wcout << result << std::endl;
+	delete[] result;//remember to delete it
+	
+	result = str.getPtr();//get the start position of the box in original C wstring
+	//because of this, it can't add a '\0' to the end
+	//so, don't use wcout or wprintf to print the result
+	//the right way is to use a ergodic
+	
+	wchar_t* end = result + str.getSize();
+	for(; result < end; result++) {
+		std::wcout << *result;
+	}
+	std::wcout << endl;
+	return 0;
+}
---
+++ example/example0.h
@@ -0,0 +1,8 @@
+﻿// example.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+#include <strongFormat.hpp>
+// TODO: 在此处引用程序需要的其他标头。
